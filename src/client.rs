a896967af059b52661a0224fb9f6fbfeafb0f03a
Baihua Chen <bchen102@myseneca.ca>
2024-11-01 13:54:15 -0400

    Finished, run build then server, then the client

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("hello client");
+use std::error::Error;
+
+use records::recorder_client::RecorderClient;
+use records::RecordRequest;
+use tonic::Request;
+
+pub mod records {
+    tonic::include_proto!("records");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut client = RecorderClient::connect("http://[::1]:50050").await?;
+    let req = Request::new(RecordRequest {
+        user_name: "myName".to_string(),
+        user_age: 28,
+    });
+    let res = client.send_message(req).await?;
+    println!("{:#?}", res);
+    println!("Metadata: \n{:#?}", res.metadata());
+    println!("Only message: \n{:#?}", res.get_ref().message);
+    Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,37 @@
+use std::error::Error;
+
+use tonic::transport::Server;
+use tonic::{self, Response};
+use records::recorder_server::{Recorder, RecorderServer};
+use records::{RecordRequest, RecordRespond};
+
 pub mod records {
     tonic::include_proto!("records");
 }
 
-fn main() {
-    println!("hello, server");
+#[derive(Debug, Default)]
+pub struct RecorderService {}
+
+#[tonic::async_trait]
+impl Recorder for RecorderService {
+    async fn send_message(&self, req: tonic::Request<RecordRequest>) -> Result<tonic::Response<RecordRespond>, tonic::Status> {
+        println!("request: {:#?}", req);
+        let reqst = req.into_inner();
+        let res = RecordRespond {
+            successful: true,
+            message: format!("User {} is {} old", reqst.user_name, reqst.user_age),
+        };
+        Ok(Response::new(res))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>>{
+    let addr= "[::1]:50050".parse()?; 
+    let recorder = RecorderService::default();
+    println!("Recorder is listening: {}", addr);
+
+    Server::builder().add_service(RecorderServer::new(recorder)).serve(addr).await?;
+
+    Ok(())
 }
